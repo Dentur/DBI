9368336948e402a398f4491c76385799aec409e7
Sebastian Venhuis <dentur007@gmail.com>
2014-11-26 15:13:15 +0100

    Leichte optimierung und aufräumen des quellcodes

--- DBI Praktikum/src/P7/P7A1a.java
+++ DBI Praktikum/src/P7/P7A1a.java
@@ -1,167 +1,125 @@
 package P7;
 
 import java.sql.*;
+import java.util.Random;
 
 public class P7A1a {
 	public static long time, time2;
-	public static long sTime, eTime,sTime2, eTime2;
+	public static long sTime2, eTime2;
 	public static int batchSize = 1000;
 	public static void main(String[] args)
 	{
-		time = 0;
-		time2=0;
-		sTime2 = System.nanoTime();
-		//Create connection string
+		Random rand = new Random();
+		//Erstelle den verbindungs String
 		String connectionURL = "jdbc:sqlserver://localhost:1433;"
 		+ "databaseName=BenchmarkDB;user=test;password=123";
 		
 		//JDBC Objects
 		Connection con = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		//Establish the connection
 		
 		try {
 			PreparedStatement pst;
+			//Lade den msjdbc treiber
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			
-			sTime = System.nanoTime();
+			//Verbiindung mit dem Server herstellen
 			con = DriverManager.getConnection(connectionURL);
-			eTime = System.nanoTime();
-			time += eTime - sTime;
 			
-			
-			//TODO N 
-			int ns[] = {10, 3, 4};
-			//int ns[] = {10, 20, 50};
-			for(int index = 0; index < 1; index++)
+			int ns[] = {10, 20, 50};
+			for(int index = 0; index < 3; index++)
 			{
-				
+				//Alte Datensätze löschen
 				String SQLL = "DELETE FROM tellers;DELETE FROM accounts;DELETE FROM branches;";
-				
 				Statement st = con.createStatement();
 				int l = st.executeUpdate(SQLL);
 				st.close();
 				
-				int n = ns[index];
-				time = 0;
-				sTime2 = System.nanoTime();
-				con.setAutoCommit(false);
+				//Start eines Durchlaufs
 				
-				sTime = System.nanoTime();
+				sTime2 = System.nanoTime();
+				//Deaktiviere auto Commit 
+				con.setAutoCommit(false);
+				int n = ns[index];
+				//Erstelle das insert Statement für die branches
 				pst = con.prepareStatement("INSERT INTO branches (branchid, branchname, balance, address) "+
 						"VALUES (?,?,?,?);");
-				eTime = System.nanoTime();
-				time += eTime - sTime;
+				//Branches mit Tupeln füllen
 				for(int i = 0; i < n; i++)
 				{
-					//String SQL = "INSERT INTO branches (branchid, branchname, balance, address) "+
-							//"VALUES ("+i+",'bcerabcerabcerabcer',0,'bcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabr');";
-					
-					
-					pst.setInt(1, i);
-					pst.setString(2,"bcerabcerabcerabcer");
-					pst.setInt(3,0);
-					pst.setString(4, "bcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabr");
+					//Setze Werte
+					pst.setInt(1, i);//Branchid
+					pst.setString(2,"bcerabcerabcerabcer");//Branchname
+					pst.setInt(3,0);//balance
+					pst.setString(4, "bcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabr");//address
+					//Füge das Tupel dem Batch hinzu
 					pst.addBatch();
+					//Führe den Batch aus wenn die batchSize erreicht wurde 
 					if((i % batchSize) == 0)
 					{
-						sTime = System.nanoTime();
 						pst.executeBatch();
-						eTime = System.nanoTime();
-						time += eTime - sTime;
 					}
 						
 				}
 				System.out.print("branches fertig \n");
-				sTime = System.nanoTime();
+				//Führe den Batch mit den rest der Tupel aus und beende die Transaction
 				pst.executeBatch();
 				con.commit();
+				//Das Prepared Statement wird geschlossen
 				pst.close();
-				eTime = System.nanoTime();
-				time += eTime - sTime;
-				
-				sTime = System.nanoTime();
+				//Erstelle das Insert Statement für die account
 				pst = con.prepareStatement("INSERT INTO accounts (accid, name, balance, branchid, address) "+
 						"VALUES (?,?,?,?,?);");
-				eTime = System.nanoTime();
-				time += eTime - sTime;
+				//Fülle accounts mit Tupeln
 				for(int i = 0; i < n*100000; i++)
 				{
-					/*String SQL = "INSERT INTO accounts (accid, name, balance, branchid, address) VALUES"+
-					 ("+i+", 'abcabcabcabcbacbacb', 0, 1, 'bcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcr');";
-					
-					sTime = System.nanoTime();
-					stmt = con.createStatement();
-					int v = stmt.executeUpdate(SQL);
-					stmt.close();
-	
-					eTime = System.nanoTime();
-					time += eTime - sTime;*/
-					pst.setInt(1, i);
-					pst.setString(2,"abcabcabcabcbacbacb");
-					pst.setInt(3,0);
-					pst.setInt(4, 0);
-					pst.setString(5, "bcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcr");
+					//Setze Werte
+					pst.setInt(1, i);//accid
+					pst.setString(2,"abcabcabcabcbacbacb");//name
+					pst.setInt(3,0);//balance
+					pst.setInt(4, rand.nextInt(n));//branchid
+					pst.setString(5, "bcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcr");//address
+					//Füge das Tupel dem Batch hinzu
 					pst.addBatch();
+					//Führe den Batch aus wenn die batchSize erreicht wurde 
 					if((i % batchSize) == 0)
 					{
-						sTime = System.nanoTime();
 						pst.executeBatch();
-						eTime = System.nanoTime();
-						time += eTime - sTime;
 					}
 				}
 				System.out.print("accounts fertig \n");
-				sTime = System.nanoTime();
+				//Führe den Batch mit den rest der Tupel aus und beende die Transaction
 				pst.executeBatch();
 				con.commit();
 				pst.close();
-				eTime = System.nanoTime();
-				time += eTime - sTime;
-				
-				sTime = System.nanoTime();
+				//Erstelle das Insert Statement der teller
 				pst = con.prepareStatement("INSERT INTO tellers (tellerid, tellername, balance, branchid, address) "+
 						"VALUES (?,?,?,?,?);");
-				eTime = System.nanoTime();
-				time += eTime - sTime;
+				//Fülle Teller mit Tupeln
 				for(int i = 0; i < n*10; i++)
 				{
-					/*String SQL = "INSERT INTO tellers (tellerid, tellername, balance, branchid, address) "+
-					 * "VALUES("+i+",'abcbaccbaacbbcaabca', 0, 1, 'bcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcr');";
-					
-					sTime = System.nanoTime();
-					stmt = con.createStatement();
-			        int v = stmt.executeUpdate(SQL);
-			        //rs.close();
-			        stmt.close();
-					eTime = System.nanoTime();
-					time += eTime - sTime;*/
-					pst.setInt(1, i);
-					pst.setString(2,"abcbaccbaacbbcaabca");
-					pst.setInt(3,0);
-					pst.setInt(4, 0);
-					pst.setString(5, "bcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcr");
+					//Setze Werte
+					pst.setInt(1, i);//Tellerid
+					pst.setString(2,"abcbaccbaacbbcaabca");//tellername
+					pst.setInt(3,0);//balance
+					pst.setInt(4, rand.nextInt(n));//branchid
+					pst.setString(5, "bcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcerabcr");//address
+					//Füge das Tupel dem Batch hinzu
 					pst.addBatch();
+					//Führe den Batch aus wenn die batchSize erreicht wurde 
 					if((i % batchSize) == 0)
 					{
-						sTime = System.nanoTime();
 						pst.executeBatch();
-						eTime = System.nanoTime();
-						time += eTime - sTime;
 					}
 				}
 				System.out.print("tellers fertig \n");
-				sTime = System.nanoTime();
+				//Führe den Batch mit den rest der Tupel aus und beende die Transaction
 				pst.executeBatch();
 				con.commit();
 				pst.close();
-				eTime = System.nanoTime();
-				time += eTime - sTime;
+				
 				System.out.print("fertig");
-				System.out.println("Zeit[ms] bei n: + "+n+": "+ time / (float)1000000);
+				//Messen der Zeit nach abschluss des befüllens
 				eTime2 = System.nanoTime();
+				//Ausgabe der Zeit
 				System.out.println("Zeit[ms] gesanntProgramm bei n: + "+n+": "+ (eTime2 - sTime2) / (float)1000000);
 			}
 			
@@ -169,11 +127,13 @@ public class P7A1a {
 		catch(Exception e){
 			e.printStackTrace();
 		}
+		//Schließe die verbindung egal was passiert
 		finally{
 			
 			if (con != null) 
 				try 
 				{ 
+					//Verbindung wird geschlossen
 					con.close();
 				} catch(Exception e) {}
 			
