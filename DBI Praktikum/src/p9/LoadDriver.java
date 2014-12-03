db86bc7744ce0c0ab0be41e90e25a820aac7d369
Dentur <dentur007@gmail.com>
2014-12-03 15:34:19 +0100

    LoadDriver verteilung abgeschlossen

--- DBI Praktikum/src/p9/LoadDriver.java
+++ DBI Praktikum/src/p9/LoadDriver.java
@@ -9,13 +9,28 @@ public class LoadDriver {
 	long warmupTime;
 	long lagTime;
 	long measureTime;
-	long cooldownTime;
+	long cooldownTime; 
+	
+	int verKonto;
+	int verEinzahlung;
+	int verAnalyse;
 	
 	Connection connection;
 	
 	public long actions;
-	
-	public LoadDriver(String conString, long warmupT, long lagT, long measureT, long cooldownT) throws SQLException
+	/**
+	 * 
+	 * @param conString String to Connect to the Database
+	 * @param warmupT	Time in ms to Execute Statements without measuring
+	 * @param lagT		Time in ms to Wait after each Stament
+	 * @param measureT	Time in ms to measure tps
+	 * @param cooldownT	Time in ms to keep running after the measurement
+	 * @param verKonto	Wahrscheinlichkeit das ein KontoStatement ausgewählt wird (verKonto in 100)
+	 * @param verEinzahlung Wahrscheinlichkeit das ein EinzahlungsStatement ausgewählt wird (verEinzahlung in 100)
+	 * @param verAnalyse Wahrscheinlichkeit das ein AnalyseStatement ausgewählt wird (verAnalyse in 100)
+	 * @throws SQLException
+	 */
+	public LoadDriver(String conString, long warmupT, long lagT, long measureT, long cooldownT, int verKonto, int verEinzahlung, int verAnalyse ) throws SQLException
 	{
 		try
 		{
@@ -45,32 +60,69 @@ public class LoadDriver {
 			cooldownTime = cooldownT;
 		else
 			cooldownTime = 0;
+		
+		if(verKonto >= 0)
+			this.verKonto = verKonto;
+		else
+			this.verEinzahlung = 0;
+		
+		if(verEinzahlung >= 0)
+			this.verEinzahlung = verEinzahlung;
+		else
+			this.verEinzahlung = 0;
+		
+		if(verAnalyse >= 0)
+			this.verAnalyse = verAnalyse;
+		else
+			this.verAnalyse = 0;
 	}
 	
+	/*
+	 * Starts the Insertloop
+	 */
 	public long start()
 	{
-		long anzahl = 0, runntime = 0, startTime, tempTime;
+		long anzahl = 0, runtime = 0, startTime, tempTime;
 		int phase = 0;//0 = warmup; 1 = measure; 3 = cooldown;
 		boolean run = true;
 		startTime = System.nanoTime();
+		Random rand = new Random();
+		int auswahl;
 		while(run)
 		{
 			tempTime = System.nanoTime();
-			runntime = (long) ((tempTime - startTime)/(float)1000000);
-			if(runntime < warmupTime)
+			runtime = (long) ((tempTime - startTime)/(float)1000000);
+			if(runtime < warmupTime)
 			{
 				phase = 0;
 			}
-			else if(runntime > measureTime + warmupTime)
+			else if(runtime > measureTime + warmupTime)
 			{
 				phase = 2;
+			}
+			else if(runtime > (measureTime + warmupTime + cooldownTime))
+			{
+				run = false;
 			}
 			else
 			{
 				phase = 1;
 			}
 			
-			
+			//Statement absetzen
+			auswahl = rand.nextInt(100) +1;
+			if( auswahl <= verKonto)
+			{
+				System.out.println("Konto");
+			}
+			else if((auswahl > verKonto) && (auswahl < (verKonto + verEinzahlung)) )
+			{
+				System.out.println("Einzahlung");
+			}
+			else if(auswahl > (verKonto + verEinzahlung) && (auswahl < (verKonto + verEinzahlung + verAnalyse))) 
+			{
+				System.out.println("Analyse");
+			}
 			
 			
 			if(phase == 1)
